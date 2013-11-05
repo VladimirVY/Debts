fe6291e15d95960f57052131f7ef2b06a966c607
VladimirVY <v-v-y@ya.ru>
2013-11-05 22:28:22 +0400

    vk

--- web/index.php
+++ web/index.php
@@ -4,8 +4,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), 
+                array('twig.path' => __DIR__.'/../views',
+));
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+
+
 $app->get('/hello', function() {
     return 'Hello!';
-});
+})
+->bind('homepage');
+
+
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('main.html',array());
+}); 
+
+
 
 $app->run();
