e779dbd517639a82d7d295d8e0cf8d788d9f4294
VladimirVY <v-v-y@ya.ru>
2013-11-05 23:14:50 +0400

    vk work

--- web/index.php
+++ web/index.php
@@ -12,7 +12,18 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 
 
-$app->get('/hello', function() {
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app->get('/hello', function (Request $request) use ($app) {
+    $code = $request->get('code');
+
+$str=file_get_contents('https://oauth.vk.com/access_token?client_id=3977669&client_secret=06aTUBK00Ee072ODRiip&code='.$code.'&redirect_uri=http://localhost'.$app['url_generator']->generate('homepage'));
+
+$str1=json_decode($str,true);
+$mass=file_get_contents('https://api.vk.com/method/friends.get?'.$str1['user_id'].'&access_token='.$str1['access_token']);
+var_dump($mass);
     return 'Hello!';
 })
 ->bind('homepage');
@@ -26,3 +37,6 @@ $app->get('/', function() use ($app) {
 
 
 $app->run();
+
+
+
